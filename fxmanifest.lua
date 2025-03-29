81008408ee3bb9532d43a2636f01d9331d84451d
MEKEZ <128120861+mekezmkz@users.noreply.github.com>
2025-03-29 04:40:42 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'  -- Add this line to enable Lua 5.4
+
+author 'MKZ DEV'
+description 'Colored Waypoint'
+version '1.0.0'
+
+client_scripts {
+    '*.lua'
+}
